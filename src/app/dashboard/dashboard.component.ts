10b1c863a4a836d0c42125459a6a52e583ee33bc
MPopov <desig9stein@gmail.com>
2020-10-26 10:33:47 +0200

    Finish fluid styles

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,104 +11,104 @@ import { health, programming } from '@igniteui/material-icons-extended';
 export class DashboardComponent {
     private productPATH = './assets/products/';
 
-    public cardDataSlide1  = [
+    public tvData = [
         {
             imageURL: this.productPATH + 'Sony-A8H-55-4K-OLED.png',
             title: 'Sony A8H 55 4K OLED',
             totalEarned: 231,
-            soldPieces: 32423,
+            soldPieces: 32423
         },
         {
             imageURL: this.productPATH + 'Sony-A8H-55-4K-OLED.png',
             title: 'Sony A8H 55 4K OLED',
             totalEarned: 331,
-            soldPieces: 32423,
+            soldPieces: 32423
         },
         {
             imageURL: this.productPATH + 'Sony-A8H-55-4K-OLED.png',
             title: 'Sony A8H 55 4K OLED',
             totalEarned: 421,
-            soldPieces: 32423,
-        },
-    ];
-
-    public cardDataSlide2  = [
-        {
-            imageURL: this.productPATH + 'Canon-RF-85mm-f1.2L.png',
-            title: 'Canon RF 85mm f1.2L',
-            totalEarned: 231,
-            soldPieces: 32423,
-        },
-        {
-            imageURL: this.productPATH + 'Canon-RF-85mm-f1.2L.png',
-            title: 'Canon RF 85mm f1.2L',
-            totalEarned: 231,
-            soldPieces: 32423,
-        },
-        {
-            imageURL: this.productPATH + 'Canon-RF-85mm-f1.2L.png',
-            title: 'Canon RF 85mm f1.2L',
-            totalEarned: 231,
-            soldPieces: 32423,
-        },
-    ];
-
-    public cardDataSlide3  = [
-        {
-            imageURL: this.productPATH + 'DJI-Mavic-Air-2.png',
-            title: 'DJI Mavic Air 2',
-            totalEarned: 231,
-            soldPieces: 32423,
-        },
-        {
-            imageURL: this.productPATH + 'DJI-Mavic-Air-2.png',
-            title: 'DJI Mavic Air 2',
-            totalEarned: 231,
-            soldPieces: 32423,
-        },
-        {
-            imageURL: this.productPATH + 'DJI-Mavic-Air-2.png',
-            title: 'DJI Mavic Air 2',
-            totalEarned: 231,
-            soldPieces: 32423,
-        },
-    ];
-
-    public category = [
-        {
-            name: 'Monitors',
-            products: this.cardDataSlide1
-        },
-        {
-            name: 'Photography',
-            products: this.cardDataSlide2
-        },
-        {
-            name: 'Drones',
-            products: this.cardDataSlide3
+            soldPieces: 32423
         }
     ];
 
-  public pieChartData = [
-    { Sale: 33, City: 'New York',    },
-    { Sale: 29, City: 'San Francisco', },
-    { Sale: 20, City: 'New Jersey', },
-    { Sale: 18, City: 'London',   }
-  ];
+    public photoData = [
+        {
+            imageURL: this.productPATH + 'Canon-EOS-R5.jpg',
+            title: 'Canon EOS R5',
+            totalEarned: 251,
+            soldPieces: 33423
+        },
+        {
+            imageURL: this.productPATH + 'Broncolor-Siros-400L.png',
+            title: 'Broncolor Siros 400L',
+            totalEarned: 221,
+            soldPieces: 31423
+        },
+        {
+            imageURL: this.productPATH + 'Canon-RF-85mm-f1.2L.png',
+            title: 'Canon RF 85mm f1.2L',
+            totalEarned: 201,
+            soldPieces: 31423
+        }
+    ];
 
-  public channelData = [
-    { Channel: 'Amazon', Orders: 37},
-    { Channel: 'Ebay', Orders: 13},
-    { Channel: 'Website', Orders: 25},
-    { Channel: 'Facebook', Orders: 25},
-  ];
+    public accessoriesData = [
+        {
+            imageURL: this.productPATH + 'Rode-Wireless-GO-Black.png',
+            title: 'Rode Wireless GO Black',
+            totalEarned: 231,
+            soldPieces: 32423
+        },
+        {
+            imageURL: this.productPATH + 'BenQ-TK850.png',
+            title: 'BenQ TK850',
+            totalEarned: 231,
+            soldPieces: 32423
+        },
+        {
+            imageURL: this.productPATH + 'GoPro-HERO8-Black.png',
+            title: 'GoPro-HERO8-Black',
+            totalEarned: 231,
+            soldPieces: 32423
+        }
+    ];
 
-  public orderStatus = [
-    { Year: 'Sep 30', approved: 220, canceled: 60 },
-    { Year: 'Oct 1', approved: 520, canceled: 80 },
-    { Year: 'Oct 2', approved: 200, canceled: 30 },
-    { Year: 'Oct 3', approved: 360, canceled: 100 },
-    { Year: 'Oct 4', approved: 490, canceled: 85 }
-  ];
+    public categories = [
+        {
+            name: 'Monitors',
+            products: this.tvData
+        },
+        {
+            name: 'Photography',
+            products: this.photoData
+        },
+        {
+            name: 'Accessories',
+            products: this.accessoriesData
+        }
+    ];
+
+    public pieChartData = [
+        {Sale: 33, City: 'New York'},
+        {Sale: 29, City: 'San Francisco'},
+        {Sale: 20, City: 'New Jersey'},
+        {Sale: 18, City: 'London'}
+    ];
+
+    public channelData = [
+        {Channel: 'Amazon', Orders: 37},
+        {Channel: 'Ebay', Orders: 13},
+        {Channel: 'Website', Orders: 25},
+        {Channel: 'Facebook', Orders: 25}
+    ];
+
+    public orderStatus = [
+        {Year: 'Sep 30', approved: 220, canceled: 60},
+        {Year: 'Oct 1', approved: 520, canceled: 80},
+        {Year: 'Oct 2', approved: 200, canceled: 30},
+        {Year: 'Oct 3', approved: 360, canceled: 100},
+        {Year: 'Oct 4', approved: 490, canceled: 85}
+    ];
 
 }
