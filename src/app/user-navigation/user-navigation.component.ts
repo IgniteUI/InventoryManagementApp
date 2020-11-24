d5686e8b8de3a74c6fb2463a383b08ad2492bc32
Deyan Kamburov <dkamburov@infragistics.com>
2020-11-24 20:46:23 +0200

    fix(themes): Apply initial theme mixin, don't wait for the response from theming api

--- src/app/user-navigation/user-navigation.component.ts
+++ src/app/user-navigation/user-navigation.component.ts
@@ -140,7 +140,7 @@ export class UserNavigationComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.resetTheme('igx-indigo-light-theme');
+        // this.resetTheme('igx-indigo-light-theme');
         document.body.classList.add(this.theme.globalTheme);
     }
 
